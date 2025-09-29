253a6f7d3f46b63d2d073d8294f42dc5a410444b
Madhav Arora <aroramadhav65@gmail.com>
2025-09-29 22:41:38 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,11 @@
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => clients.claim());
-
-self.addEventListener('push', e => {
-  const data = e.data?.json() || {};
+self.addEventListener('install', e=>self.skipWaiting());
+self.addEventListener('activate', e=>self.clients.claim());
+self.addEventListener('push', e=>{
+  const data=e.data?.json()||{};
   e.waitUntil(
-    self.registration.showNotification(data.title || 'Medicine Reminder', {
-      body: data.body || 'Time to take your medicine',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2966/2966484.png'
+    self.registration.showNotification(data.title||'Medicine Reminder',{
+      body:data.body||'Time to take your medicine',
+      icon:'icon-192.png'
     })
   );
 });
